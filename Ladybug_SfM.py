b218d1806ff12a6e8af72b9be18247bb9576cbd2
Jay W Johnson <tastethejava@hotmail.com>
2012-09-15 01:14:10 +0800

    Update Ladybug_SfM.py

--- Ladybug_SfM.py
+++ Ladybug_SfM.py
@@ -17,13 +17,13 @@
 
 @AUTHOR: Ripley6811
 @ORGANIZATION: National Cheng Kung University, Department of Earth Sciences
-@CONTACT: tastethejava@hotmail.com
+@CONTACT: python at boun.cr
 """
 #===============================================================================
 # PROGRAM METADATA
 #===============================================================================
 __author__ = 'Ripley6811'
-__contact__ = 'tastethejava@hotmail.com'
+__contact__ = 'python at boun.cr'
 __copyright__ = ''
 __license__ = ''
 __date__ = 'Sat Aug 04 21:45:58 2012'
