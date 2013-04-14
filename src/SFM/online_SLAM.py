41c4b017142f6c8a2b0a21b8955133e10aad1c69
Jay W Johnson <tastethejava@hotmail.com>
2013-04-14 15:04:57 +0800

    SLAM updated
    Working. All returns are NumPy arrays instead of matrices. Better
    documentation.

--- src/SFM/online_SLAM.py
+++ src/SFM/online_SLAM.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-(SUMMARY)
+SLAM implemented with NumPy
 
-(DESCRIPTION)
+Bundle Adjustment of subject positions and landmark positions. Based on online SLAM
+introduced in Udacity's self-driving robot course. The number of dimensions handled
+is chosen by the initial point passed to the constructor.
 
-:REQUIRES: ...
-:PRECONDITION: ...
-:POSTCONDITION: ...
+:REQUIRES: NumPy
+:TODO: 
+    - Change to manage ID's within this class. Map entered ID's to an index
+    position. Prevent errors from non-contiguous ID numbers.
+    - Add option to submit measurement confidence along with measurement.
 
 :AUTHOR: Ripley6811
 :ORGANIZATION: National Cheng Kung University, Department of Earth Sciences
 :CONTACT: python@boun.cr
 :SINCE: Thu Mar 14 19:25:12 2013
-:VERSION: 0.1
-:STATUS: Nascent
-:TODO: ...
+:VERSION: 1.0
+:STATUS: Working
 """
 #===============================================================================
 # PROGRAM METADATA
@@ -25,27 +28,16 @@ __contact__ = 'python@boun.cr'
 __copyright__ = ''
 __license__ = ''
 __date__ = 'Thu Mar 14 19:25:12 2013'
-__version__ = '0.1'
+__version__ = '1.0'
 
-#__package__ = 'SFM'
 
 #===============================================================================
 # IMPORT STATEMENTS
 #===============================================================================
-from numpy import *  # IMPORTS ndarray(), arange(), zeros(), ones()
+import numpy as np  # IMPORTS ndarray(), arange(), zeros(), ones()
 #set_printoptions(precision=5)
-set_printoptions(suppress=True)
-#from visual import *  # IMPORTS NumPy.*, SciPy.*, and Visual objects (sphere, box, etc.)
-#import matplotlib.pyplot as plt  # plt.plot(x,y)  plt.show()
-#from pylab import *  # IMPORTS NumPy.*, SciPy.*, and matplotlib.*
-#import os  # os.walk(basedir) FOR GETTING DIR STRUCTURE
-#import pickle  # pickle.load(fromfile)  pickle.dump(data, tofile)
-#from tkFileDialog import askopenfilename, askopenfile
-#from collections import namedtuple
-#from ctypes import *
-#import glob
-#import random
-#import cv2
+np.set_printoptions(suppress=True)
+
 
 #===============================================================================
 # METHODS
@@ -55,10 +47,13 @@ set_printoptions(suppress=True)
 class SLAM:
     '''SLAM matrix management.
 
-    Methods:
-        add_measurement for distance estimation to point(s).
-        add_motion for new estimated motion from previous position.
-        get_positions for the SLAM results of all available positions and pts.
+    :Methods:
+        *add_measurement* for distance estimation to point(s).
+        *add_motion* for new estimated motion from previous position.
+        *get_positions* for the SLAM results of all tracked positions.
+        *get_landmarks* for the SLAM results of all landmarks.
+        *get_pt_hist* for all archived positions.
+        *remove_pos* to remove oldest positions from matrix.
 
     Measurements is a list of two to four value array.
     First value is a ID number for the measured point.
@@ -66,18 +61,28 @@ class SLAM:
     number of dimensions.
     Position is a one to three value array corresponding to x,y,z.
     '''
-    def __init__(self, pos=zeros(3, float),
+    def __init__(self, pos=np.zeros(3, float),
                  motion_confidence=1.0, measurement_confidence=1.0):
         '''Initialize SLAM matrices.
 
         Initialize with a starting position of any dimension. Defaults to
         3-dimensions with starting point of zeros.
+        
+        :type pos: iterable type with floats
+        :arg  pos: Initial starting position. Sets dimensionality.
+        :type motion_confidence: Float
+        :arg  motion_confidence: Default motion reliability, smaller
+            means more reliable
+        :type measurement_confidence: Float
+        :arg  measurement_confidence: Default measurement reliability, 
+            smaller means more reliable
         '''
         self.nPts = 0 # number of correspondings points / landmarks
         self.dim = len(pos)  # number of dimensions to use (x,y,z)
         self.landmarkID = [] # number of landmarks entered into matrix
         self.motion_confidence = motion_confidence
         self.measurement_confidence = measurement_confidence
+        self.pts_history = [] # Store positions removed from matrix
 
         mat_len = 1 + self.nPts
 
@@ -85,23 +90,28 @@ class SLAM:
         self.Xi = []
         for i, xyz in enumerate(pos):
         # make the constraint information matrix and vector
-            self.Omega.append( asmatrix(zeros( (mat_len, mat_len), float ) ) )
+            self.Omega.append( np.asmatrix(np.zeros( (mat_len, mat_len), float ) ) )
             self.Omega[i][0,0] = 1.0
 
-            self.Xi.append( asmatrix(zeros( (mat_len, 1), float ) ) )
+            self.Xi.append( np.asmatrix(np.zeros( (mat_len, 1), float ) ) )
             self.Xi[i][0,0] = xyz
 
         self.nPos = 1  # number of positions entered into matrix
 
+        
 
     def add_measurement(self, pts):
         '''Adds measurement data to most recent motion.
 
-        Format: List of tuples/lists: [[ID, x, y, z], [ID, x, y, z],...].
-        Can be 1 or more dimensions.
-        Increases the size of the array for new point ID's.
+        Increases the size of the array if new point ID's are found.
         New landmarks are added to end of row and column.
+        ID numbers must start from zero and be managed outside of this class.
+        :TODO: Manage IDs within class by mapping, this will avoid problems.
+        
+        :type pts: List of lists or other iterable type
+        :arg  pts: List of measurements. Each like [ID, x[, y[, z]]].
         '''
+        self.updated = False
         P = self.nPos - 1
         LIDs = self.landmarkID
 
@@ -119,10 +129,10 @@ class SLAM:
                     # Create shortcut to desired dimension arrays
                     O = self.Omega[xyz]
                     X = self.Xi[xyz]
-                    O2 = mat(zeros(array(O.shape)+1))
+                    O2 = np.mat(np.zeros(np.array(O.shape)+1))
                     O2[:-1,:-1] = O[:,:]
 
-                    X2 = mat(zeros((X.shape[0]+1, 1)))
+                    X2 = np.mat(np.zeros((X.shape[0]+1, 1)))
                     X2[:-1,0] = X[:,0]
 
                     # Copy new matrices over global arrays
@@ -147,17 +157,17 @@ class SLAM:
 
 
 
-
-
     def add_motion(self, motion):
-        '''Adds motion from last position.
+        '''Add a motion since last position.
 
-        Motion format: Single tuple/list: [x, y, z].
-        Can be 1 or more dimensions.
-        Increases the size of each array by one. New row and column inserted
+        Increases the size of matrix by one. New row and column inserted
         between positional and landmark sections.
+        
+        :type motion: Iterable type containing floating point numbers
+        :arg  motion: Movement in one or more dimensions.
         '''
         assert len(motion) == self.dim
+        self.updated = False
 
         N = self.nPos
 
@@ -168,13 +178,13 @@ class SLAM:
 
             # Create expanded matrices and copy data
             # Insert new col and row between position and landmark sections
-            O2 = mat(zeros(array(O.shape)+1))
+            O2 = np.mat(np.zeros(np.array(O.shape)+1))
             O2[:N,:N] = O[:N,:N]
             O2[:N,N+1:] = O[:N,N:]
             O2[N+1:,:N] = O[N:,:N]
             O2[N+1:,N+1:] = O[N:,N:]
 
-            X2 = mat(zeros((X.shape[0]+1, 1)))
+            X2 = np.mat(np.zeros((X.shape[0]+1, 1)))
             X2[:N,0] = X[:N,0]
             X2[N+1:,0] = X[N:,0]
 
@@ -194,29 +204,77 @@ class SLAM:
 
 
 
-
-
-
-
-    def get_positions(self):
-        '''Returns the updated positions for available positions and points.
-
+    def calculate(self):
+        '''Solves the matrix to find optimal pts.
+        
+        Solves the equation: Omega.I * Xi = Mu
+        Maintains a list of updated positions and landmarks.
         '''
         mu = []
         for O,X in zip(self.Omega, self.Xi):
             mu.append( O.I * X )
 
-        return hstack(mu)
+        stacked = np.hstack(mu)
+        self.updated_positions = stacked[:self.nPos].copy()
+        self.updated_landmarks = stacked[self.nPos:].copy()
+
+        self.updated = True
+
+        
+
+    def get_positions(self):
+        '''Returns the updated positions for those in the matrix.
+
+        :rtype: NumPy array [points, dimensions]
+        '''
+        if not self.updated:
+            self.calculate()
+
+        return np.array(self.updated_positions)
+
+        
+
+    def get_landmarks(self):
+        '''Returns the updated landmarks for those in the matrix.
+
+        :rtype: NumPy array [points, dimensions]
+        '''
+        if not self.updated:
+            self.calculate()
+
+        return np.array(self.updated_landmarks)
+        
+        
+        
+    def get_pt_hist(self):
+        '''Returns the list of archived points as a NumPy array.
+        
+        The archived points are stored in a list within the class.
+        This method simply converts to an np.array.
+        
+        :rtype: NumPy array [points, dimensions]
+        '''
+        return np.array(self.pts_history) 
 
 
-
+        
     def remove_pos(self, number_to_remove=1):
         '''Removes oldest positions.
 
+        Points are removed from the matrix and added to a list. They
+        are not updated after removal.
+        
+        :type number_to_remove: int
+        :arg  number_to_remove: Number of (oldest) points to remove/archive.
         '''
         # Most recent motion cannot be removed
         if number_to_remove >= self.nPos:
             return False
+            
+        if not self.updated:
+            self.calculate()
+            
+        pts = self.updated_positions[:number_to_remove]
 
         for xyz in range(self.dim):
             n = number_to_remove
@@ -238,12 +296,7 @@ class SLAM:
             self.Omega[xyz] = O
             self.Xi[xyz] = X
 
+        for each in np.array(pts):
+            self.pts_history.append( each )
+        self.updated = False
         self.nPos -= number_to_remove
-
-
-if __name__ == "__main__" and __package__ is None:
-    __package__ = "online_SLAM"
-
-
-
-
--- src/SFM/test/test_online_SLAM.py
+++ src/SFM/test/test_online_SLAM.py
@@ -30,9 +30,10 @@ __version__ = '0.1'
 #__package__  = 'SFM'
 
 #export PYTHONPATH=..
+import numpy as np
 import sys
 sys.path.append("..") # Access modules that are one level up
-
+import matplotlib.pyplot as plt
 from online_SLAM import SLAM
 
 
@@ -58,15 +59,25 @@ def test_data1():
 
 def test_data2():
         # Actual motion from starting point of (0,0,0)
-    motions = [[1.,-1.,0.],
-               [1.,1.,1.],
-               [1.,1.,-1.],
-               [1.,-1.,-.5],
-               [1.,-.5,0.6]]
+    initial = [0.,0.,0.]
+    motions = [[0.,-1.,0.],
+               [1.,-1.,1.],
+               [1.5,-.5,-1.],
+               [1.,0.,-.5],
+               [0.,0,0.6]]
 
     # Actual landmark positions relative to starting point
     landmarks = [[0.,8.,0.2],
-                 [6.,10.2,-0.1]]
+                 [6.,10.2,-0.1],
+                 [-1,-34,0.02],
+                 [2,-10,0.1],
+                 [29,102,3],
+                 [-31,45.,21],
+                 [6.,5.2,-4.1],
+                 [-1,-34.5,0.22],
+                 [2,-10,2.1],
+                 [24,12,3],
+                 [-36,25.,61]]
 
     # Real distances as measurements
     curr_position = [0.,0.,0.]
@@ -74,10 +85,15 @@ def test_data2():
     for each in motions:
         landmark_distances = []
         for l, landmark in enumerate(landmarks):
+#            landmark_distances.append([l,
+#                                       landmark[0]-curr_position[0],
+#                                       landmark[1]-curr_position[1],
+#                                       landmark[2]-curr_position[2]])
+            # landmarks not moving
             landmark_distances.append([l,
-                                       landmark[0]-curr_position[0],
-                                       landmark[1]-curr_position[1],
-                                       landmark[2]-curr_position[2]])
+                                       landmark[0],
+                                       landmark[1],
+                                       landmark[2]])
         measurements.append(landmark_distances)
         curr_position = [curr_position[0] + each[0],
                          curr_position[1] + each[1],
@@ -104,7 +120,7 @@ def main():
     """Description of main()"""
     motions, measurements = test_data1()
 
-    slam = SLAM((50.,50.), 3.0, 5.0)
+    slam = SLAM((50.,50.), 2.0, 2.0)
     slam.add_measurement(measurements[0])
     slam.add_motion(motions[0])
     slam.add_measurement(measurements[1])
@@ -123,22 +139,38 @@ def main():
 
 
     motions, measurements = test_data2()
-    slam = SLAM()
+    slam = SLAM((0.,0.,0.), 0.1, 0.1)
     slam.add_measurement(measurements[0])
     slam.add_motion(motions[0])
+#    slam.remove_pos()
     slam.add_measurement(measurements[1])
     slam.add_motion(motions[1])
+#    slam.remove_pos()
     slam.add_measurement(measurements[2])
     slam.add_motion(motions[2])
+#    slam.remove_pos()
     slam.add_measurement(measurements[3])
     slam.add_motion(motions[3])
+#    slam.remove_pos()
     slam.add_measurement(measurements[4])
     slam.add_motion(motions[4])
-    slam.remove_pos(3)
-    print slam.Omega[0]
+    slam.remove_pos()
+#    slam.remove_pos(3)
     print slam.Xi[0]
-    print slam.get_positions()
-
+#    print slam.get_positions()
+    cres = slam.get_positions()
+    print 'returned positions'
+    print slam.get_pt_hist()
+    print cres
+#    print slam.Omega[0]
+#    print slam.Xi[0]
+#    print slam.Xi[1]
+#    print slam.Xi[2]
+    plt.plot(*np.cumsum(np.array(motions)[:,:2],0).T)
+    plt.plot(*cres[:,:2].T, linewidth=3)
+#    print np.cumsum(np.array(motions),0)
+#    print cres[1:,:2]
+    plt.show()
 
 if __name__ == '__main__':
     main()
